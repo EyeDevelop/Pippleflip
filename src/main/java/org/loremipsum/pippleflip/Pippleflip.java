b553a3016d7e535ae090ec25ec74d1305c835091
Hans Goor <rooteddev@outlook.com>
2019-11-14 16:40:48 +0100

    Added required hooks and base command.

--- src/main/java/org/loremipsum/pippleflip/Pippleflip.java
+++ src/main/java/org/loremipsum/pippleflip/Pippleflip.java
@@ -1,9 +1,31 @@
 package org.loremipsum.pippleflip;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import org.loremipsum.pippleflip.commands.pf.CmdPippleflip;
 
 public class Pippleflip extends JavaPlugin {
 
+    @Override
+    public void onEnable() {
+        super.onEnable();
 
+        // Load commands.
+        getServer().getPluginCommand("pippleflip").setExecutor(new CmdPippleflip());
 
+        // Notify that the plugin is loaded successfully.
+        getLogger().info("PippleFlip is up and running!");
+    }
+
+    @Override
+    public void onDisable() {
+        super.onDisable();
+
+        // Notify the plugin is disabled.
+        getLogger().info("PippleFlip is disabled.");
+    }
+
+    @Override
+    public void onLoad() {
+        super.onLoad();
+    }
 }
---
+++ src/main/java/org/loremipsum/pippleflip/commands/pf/CmdPippleflip.java
@@ -0,0 +1,14 @@
+package org.loremipsum.pippleflip.commands.pf;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class CmdPippleflip implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
+        return false;
+    }
+
+}
